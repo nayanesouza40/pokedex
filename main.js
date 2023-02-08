54c1a5bbddd4c72a1840197fa7a14b6846a59dc6
Nayane Souza <113534680+nayanesouza40@users.noreply.github.com>
2023-02-08 19:48:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,95 +1,84 @@
-/*
-// função que faz requisição dos dados detalhados de casa pokemon
-async function displayPokemonData(pokemonName) {
-    const pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-      .then(res => res.json());
-  
-    const pokemonContainer = document.getElementById("pokemon-container");
-  
-    const pokemonElement = document.createElement("div");
-    pokemonElement.classList.add("pokemon");
-  
-    const pokemonNameElement = document.createElement("h2");
-    pokemonNameElement.innerText = pokemonData.name;
-    pokemonElement.appendChild(pokemonNameElement);
-  
-    const pokemonNumberElement = document.createElement("p");
-    pokemonNumberElement.innerText = `Número: ${pokemonData.id}`;
-    pokemonElement.appendChild(pokemonNumberElement);
-  
-    const pokemonTypesElement = document.createElement("p");
-    pokemonTypesElement.innerText = `Tipos: ${pokemonData.types.map(type => type.type.name).join(", ")}`;
-    pokemonElement.appendChild(pokemonTypesElement);
-  
-    const pokemonAbilitiesElement = document.createElement("p");
-    pokemonAbilitiesElement.innerText = `Habilidades: ${pokemonData.abilities.map(ability => ability.ability.name).join(", ")}`;
-    pokemonElement.appendChild(pokemonAbilitiesElement);
-  
-
-    const pokemonAboutElement = document.createElement("p");
-    const flavorTextEntries = pokemonData.species && pokemonData.species.flavor_text_entries;
-    const flavorTextEntry = flavorTextEntries && flavorTextEntries.find(entry => entry.language.name === "en");
-pokemonAboutElement.innerText = flavorTextEntry ? `Sobre: ${flavorTextEntry.flavor_text}` : "Sobre: não disponível";
-pokemonElement.appendChild(pokemonAboutElement);
-
-
-
-    const pokemonRegionElement = document.createElement("p");
-    pokemonRegionElement.innerText = `Região: ${pokemonData.location_area_encounters.split("/")[6]}`;
-    pokemonElement.appendChild(pokemonRegionElement);
-
-   
-    
-  
-    const pokemonImageElement = document.createElement("img");
-    pokemonImageElement.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png`;
-    pokemonElement.appendChild(pokemonImageElement);
-  
-    pokemonContainer.appendChild(pokemonElement);
-  }
-
-*/
-
-// função requisição de todos os pokemons
-  async function getAllPokemonData() {
+const getAllPokemonData = async () => {
     const url = `https://pokeapi.co/api/v2/pokemon?limit=151`;
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      return data.results;
-    } catch (error) {
-      console.error(error);
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data.results;
+  
+      } catch (error) {
+        console.error(error);
+      }
     }
-  }
 
-  // função que exibi todos os pokemons na tela
-  async function displayAllPokemonData() {
+    let cor = 'lightblue';
+
+    
+    const displayAllPokemonData = async () => {
     const pokemonList = await getAllPokemonData();
     const pokemonContainer = document.getElementById("pokemon-container");
-  
+    
     pokemonList.forEach(async pokemon => {
-      const pokemonData = await fetch(pokemon.url).then(res => res.json());
-  
-      const pokemonElement = document.createElement("div");
-      pokemonElement.classList.add("pokemon");
-      pokemonElement.classList.add("col-3")
+        const pokemonData = await fetch(pokemon.url).then(res => res.json());
+      
+        const pokemonElement = document.createElement("div");
+        pokemonElement.classList.add("pokemon-flex");
+        pokemonElement.classList.add("col-2")
 
-      pokemonElement.innerHTML = `
-        <p>${pokemonData.id}</p>
-        <h2>${pokemon.name}</h2> 
-        <p>${pokemonData.types.map(type => type.type.name).join(", ")}</p>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png"/>     
-      `
-        /*
-      pokemonNameElement.addEventListener('click', (event) => {
-        event.preventDefault();
-        console.log('clicou no '+ pokemon.name)
-        displayPokemonData(pokemon.name)
-      })
-      */
+        let [tipo1, tipo2] = pokemonData.types.map(type => type.type.name)
+        if (tipo2 == undefined) {tipo2 = ''}
 
-      pokemonContainer.appendChild(pokemonElement);
+        if(tipo1 == 'bug') {
+            cor = '#9DC130';
+        } else if(tipo1 == 'dark') {
+            cor = '#5F606D';
+        } else if (tipo1 == 'dragon') {
+            cor = '#0773C7';
+        } else if (tipo1 == 'electric') {
+            cor = '#F2D544';
+        } else if (tipo1 == 'fairy') {
+            cor = '#EF97E6'
+        } else if (tipo1 == 'fighting') {
+            cor = '#D94256'
+        } else if (tipo1 == 'fire') {
+            cor = '#F8A54F'
+        } else if (tipo1 == 'flying') {
+            cor = '#9BB4E8'
+        } else if (tipo1 == 'ghost') {
+            cor = '#6970C5'
+        } else if (tipo1 == 'grass') {
+            cor = '#5DBE62'
+        } else if (tipo1 == 'ground') {
+            cor = '#D78555'
+        } else if (tipo1 == 'ice') {
+            cor = '#7ED4C9'
+        } else if (tipo1 == 'normal') {
+            cor = '#9A9DA1'
+        } else if (tipo1 == 'poison') {
+            cor = '#B563CE'
+        } else if (tipo1 == 'psychic') {
+            cor = '#F87C7A'
+        } else if (tipo1 == 'rock') {
+            cor = '#CEC18C'
+        } else if (tipo1 == 'stell') {
+            cor = '#5596A4'
+        } else if (tipo1 == 'water') {
+            cor = '#559EDF'
+        }
+
+        pokemonElement.innerHTML = `
+            <div class="pokemon-card" style="background-color:${cor}">
+                <p class="pokemon-number">${pokemonData.id}</p>
+                <div class="pokemon-infos">
+                    <p class="pokemon-name">${pokemon.name}</p>
+                    <p class="pokemon-types">${tipo1}</p>
+                    <p class="pokemon-types" class="tipo2">${tipo2}</p>
+                </div>           
+                <img class="pokemon-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png"/>                
+            </div>   
+            `
+    
+        pokemonContainer.appendChild(pokemonElement);
     });
-  }
-  
-  displayAllPokemonData()
+}
+    
+displayAllPokemonData()
