3043b252e6e9a853d323e6388fabfb168d1e94c4
Nayane Souza <113534680+nayanesouza40@users.noreply.github.com>
2023-02-12 16:21:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -10,9 +10,87 @@ const getAllPokemonData = async () => {
       }
     }
 
-    let cor = 'lightblue';
+const clearPokemon = () => {
+    const pokemonElement = document.querySelectorAll('.pokemon-escuro')
+    pokemonElement.forEach(div => div.parentNode.removeChild(div))
+}
 
-    const displayAllPokemonData = async () => {
+const getPokemonInfo = async (pokemonName) => {
+    const pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        .then(res => res.json())
+        .catch(error => console.error(error));
+  
+    let [tipo1, tipo2] = pokemonData.types.map(type => type.type.name);
+
+    if (tipo2 == undefined) {
+        tipo2 = ''}
+
+    let [habilidade1, habilidade2] = pokemonData.abilities.map(ability => ability.ability.name);
+  
+    const pokemonContainer = document.getElementById("pokemon-container");
+    const pokemonElement = document.createElement("div");
+    pokemonElement.classList.add("pokemon-escuro");
+
+    pokemonElement.innerHTML = `
+        <div class="pokemon-pop" style="background-color:${whatsColor(tipo1)}">
+            <p onClick="clearPokemon()">x</p>
+            <img class="pokemon-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png"/>   
+            <p>${pokemonData.id}</p>
+            <p>${pokemonName}</p>
+            <p>${tipo1}</p>
+            <p>${tipo2}</p>
+            <p>${habilidade1}</p>
+            <p>${habilidade2}</p>
+        </div>
+    `
+    pokemonContainer.appendChild(pokemonElement);
+
+  }
+
+const whatsColor = (color) => {
+    let cor = 'lightblue';  
+
+        if(color == 'bug') {
+            cor = '#9DC130';
+        } else if(color == 'dark') {
+            cor = '#5F606D';
+        } else if (color == 'dragon') {
+            cor = '#0773C7';
+        } else if (color == 'electric') {
+            cor = '#F2D544';
+        } else if (color == 'fairy') {
+            cor = '#EF97E6'
+        } else if (color == 'fighting') {
+            cor = '#D94256'
+        } else if (color == 'fire') {
+            cor = '#F8A54F'
+        } else if (color == 'flying') {
+            cor = '#9BB4E8'
+        } else if (color == 'ghost') {
+            cor = '#6970C5'
+        } else if (color == 'grass') {
+            cor = '#5DBE62'
+        } else if (color == 'ground') {
+            cor = '#D78555'
+        } else if (color == 'ice') {
+            cor = '#7ED4C9'
+        } else if (color == 'normal') {
+            cor = '#9A9DA1'
+        } else if (color == 'poison') {
+            cor = '#B563CE'
+        } else if (color == 'psychic') {
+            cor = '#F87C7A'
+        } else if (color == 'rock') {
+            cor = '#CEC18C'
+        } else if (color == 'stell') {
+            cor = '#5596A4'
+        } else if (color == 'water') {
+            cor = '#559EDF'
+        }
+        return cor
+}  
+
+const displayAllPokemonData = async () => {
     const pokemonList = await getAllPokemonData();
     const pokemonContainer = document.getElementById("pokemon-container");
     
@@ -25,43 +103,7 @@ const getAllPokemonData = async () => {
 
         let [tipo1, tipo2] = pokemonData.types.map(type => type.type.name);
 
-        if(tipo1 == 'bug') {
-            cor = '#9DC130';
-        } else if(tipo1 == 'dark') {
-            cor = '#5F606D';
-        } else if (tipo1 == 'dragon') {
-            cor = '#0773C7';
-        } else if (tipo1 == 'electric') {
-            cor = '#F2D544';
-        } else if (tipo1 == 'fairy') {
-            cor = '#EF97E6'
-        } else if (tipo1 == 'fighting') {
-            cor = '#D94256'
-        } else if (tipo1 == 'fire') {
-            cor = '#F8A54F'
-        } else if (tipo1 == 'flying') {
-            cor = '#9BB4E8'
-        } else if (tipo1 == 'ghost') {
-            cor = '#6970C5'
-        } else if (tipo1 == 'grass') {
-            cor = '#5DBE62'
-        } else if (tipo1 == 'ground') {
-            cor = '#D78555'
-        } else if (tipo1 == 'ice') {
-            cor = '#7ED4C9'
-        } else if (tipo1 == 'normal') {
-            cor = '#9A9DA1'
-        } else if (tipo1 == 'poison') {
-            cor = '#B563CE'
-        } else if (tipo1 == 'psychic') {
-            cor = '#F87C7A'
-        } else if (tipo1 == 'rock') {
-            cor = '#CEC18C'
-        } else if (tipo1 == 'stell') {
-            cor = '#5596A4'
-        } else if (tipo1 == 'water') {
-            cor = '#559EDF'
-        }
+        let color = tipo1;
 
         tipo1= `<span class="tp">${tipo1}</span>`;
 
@@ -71,10 +113,11 @@ const getAllPokemonData = async () => {
             }
 
         pokemonElement.innerHTML = `
-            <div class="pokemon-card" style="background-color:${cor}">
+            <div class="pokemon-card" style="background-color:${whatsColor(color)}" onClick="getPokemonInfo('${pokemon.name}');
+            ">
                 <p class="pokemon-number">${pokemonData.id}</p>
                 <div class="pokemon-infos">
-                    <p class="pokemon-name"><a>${pokemon.name}</a></p>
+                    <a class="${pokemon.name} pokemon-name">${pokemon.name}</a>
                     <p class="pokemon-types">${tipo1}</p>
                     <p class="pokemon-types">${tipo2}</p>
                 </div>           
@@ -84,7 +127,6 @@ const getAllPokemonData = async () => {
         pokemonContainer.appendChild(pokemonElement);
     });
 }
+    
 
 displayAllPokemonData();
-
-
